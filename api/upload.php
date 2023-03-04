3cf3f98de1904caa53d0d37c50466d1f3f16b605
vasll <valentinosoll17@gmail.com>
2023-03-04 14:28:11 +0100

    Changed variable names Added gitignore

--- api/upload.php
+++ api/upload.php
@@ -1,18 +1,18 @@
 <?php
   session_start();
   
-  // Controllo se l'user è loggato
+  // Check if user is logged
   if(!isset($_SESSION['id_user'])){
-    // echo "User is not logged in. Login first.";
+    // TODO
     exit();
   }
 
   $target_dir = "uploads/".$_SESSION['id_user']."/";  // 'uploads/<id_user>/'
   $full_file_name = $_FILES["fileToUpload"]["name"];  // 'image.png'
-  $target_file = $target_dir.basename($full_file_name); // Appendo il nome del file alla dir
-  $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION)); // Prendo l'estensione del file
+  $target_file_path = $target_dir.basename($full_file_name); // 'uploads/<id_user>/image.png'
+  $file_extension = strtolower(pathinfo($target_file_path,PATHINFO_EXTENSION)); // '.png'
 
-  // Controllo se la dir esiste, altrimenti la creo
+  // Check if dir exists, if not create it
   if (!file_exists($target_dir)) {
     mkdir($target_dir, 0777, true);
   }
@@ -20,58 +20,57 @@
   // Check if image file is a actual image or fake image
   if(isset($_POST["submit"])) {
     if(empty($_FILES["fileToUpload"]["tmp_name"])){
-      // echo "Dir or file is empty.<br>";
+      // TODO (No file attached)
       exit();
     }
 
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]); // Se getimagesize() ritorna false significa che il file non è un immagine
     if($check != false) {  // Il file è un immagine
 
-    } else {  // Il file non è un immagine
-      // echo "File is not an image.<br>";
+    } else {
+      // TODO (File is not an image)
       exit();
     }
   }
   
-  // Controllo se il file esiste di già, se esiste creo un file rinominato 'filename_N.png'
-  if (file_exists($target_file)) {
-    $file_count = 1;
-
+  // Check if file exists aleady, if it does rename it to 'filename_N.png' where N is count of duplicate names
+  if (file_exists($target_file_path)) {
+    $file_count = 1;  // Counter of files with same name
     $file_info = pathinfo($full_file_name);
-    $file_name = $file_info['filename'];
+    $file_name = $file_info['filename'];  // 
 
     while(true){
-      $next_file = $file_name.'_'.$file_count.'.'.$file_info['extension'];  // 'image_3.png'
+      $next_file = $file_name.'_'.$file_count.'.'.$file_info['extension'];  // 'filename_1.png'
       $next_file_path = $target_dir.$next_file;
-      if(file_exists($next_file_path)){
+
+      if(file_exists($next_file_path)){ // 'filename_1.png' exists already, make 'filename_2.png'
         $file_count += 1;
         continue;
       }else{
         $full_file_name = $next_file;
-        $target_file = $target_dir.$next_file;
+        $target_file_path = $target_dir.$next_file;
         break;
       }
     }
-
   }
   
-  // Controllo sulla dimensione del file
-  if ($_FILES["fileToUpload"]["size"] > 10000000) {  // 10 MB
-    // echo "Sorry, your file is too large.<br>";
+  // Filesize check
+  if ($_FILES["fileToUpload"]["size"] > 10000000) {  // 12 MB
+    // TODO (File is too large)
     exit();
   }
   
   // Allow certain file formats
-  if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif"){
-    // echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+  if($file_extension != "jpg" && $file_extension != "png" && $file_extension != "jpeg" && $file_extension != "gif"){
+    // TODO (Only JPG, JPEG, PNG & GIF files are allowed)
     exit();
   }
   
-  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-    // echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file_path)) {
+    // TODO upload successful
     echo $full_file_name;
   } else {
-    // echo "Sorry, there was an error uploading your file.";
+    // TODO error with upload
   }
 
 ?>
